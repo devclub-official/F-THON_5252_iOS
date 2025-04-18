a7a5b350bcdd367d915224cac35f716d3ed13642
woojin <woojin1900@gmail.com>
2025-04-19 01:06:07 +0900

    feat: add Color extension

---
+++ OEOE/OEOE/Utils/Extensions/Color+.swift
@@ -0,0 +1,32 @@
+//
+//  Color+.swift
+//  OEOE
+//
+//  Created by 송우진 on 4/19/25.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: Double
+        switch hex.count {
+        case 6: // RRGGBB
+            r = Double((int >> 16) & 0xFF) / 255.0
+            g = Double((int >> 8) & 0xFF) / 255.0
+            b = Double(int & 0xFF) / 255.0
+        case 8: // AARRGGBB
+            r = Double((int >> 16) & 0xFF) / 255.0
+            g = Double((int >> 8) & 0xFF) / 255.0
+            b = Double(int & 0xFF) / 255.0
+        default:
+            r = 1.0
+            g = 1.0
+            b = 1.0
+        }
+        self.init(red: r, green: g, blue: b)
+    }
+}
