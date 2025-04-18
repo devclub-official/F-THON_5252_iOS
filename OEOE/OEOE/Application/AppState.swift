68224f86ae861e155296fa15141f5c716dd29c31
woojin <woojin1900@gmail.com>
2025-04-19 00:51:25 +0900

    feat: add AppState
    - 데이터 전역 관리

---
+++ OEOE/OEOE/Application/AppState.swift
@@ -0,0 +1,23 @@
+//
+//  AppState.swift
+//  OEOE
+//
+//  Created by 송우진 on 4/19/25.
+//
+
+
+import SwiftUI
+import Combine
+
+final class AppState: ObservableObject {
+    @Published var selectedTab: TabType = .home
+    
+    
+    init() {
+    }
+    
+    // 현재 탭 변경
+    func changeTab(_ type: TabType) {
+        selectedTab = type
+    }
+}
