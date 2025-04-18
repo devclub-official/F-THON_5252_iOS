b8f0c6014a22d7ae6a8cf6fac22a76199b2cfe00
woojin <woojin1900@gmail.com>
2025-04-18 23:12:37 +0900

    Create Project

---
+++ OEOE/OEOE/OEOEApp.swift
@@ -0,0 +1,17 @@
+//
+//  OEOEApp.swift
+//  OEOE
+//
+//  Created by 송우진 on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct OEOEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
